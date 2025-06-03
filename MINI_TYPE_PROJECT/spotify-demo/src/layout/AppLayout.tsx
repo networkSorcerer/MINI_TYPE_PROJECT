ee4dc56f7fcb71eff15df14abd40a160ddcdaf81
networkSocerer <gigattfyhig@gmail.com>
2025-06-03 22:32:48 +0900

    week2 day2

--- MINI_TYPE_PROJECT/spotify-demo/src/layout/AppLayout.tsx
+++ MINI_TYPE_PROJECT/spotify-demo/src/layout/AppLayout.tsx
@@ -73,11 +73,13 @@ const LibraryHead = styled(Box)(({ theme }) => ({
 }));
 
 const LibraryHelp = styled(Box)(({ theme }) => ({
+  alignItems: "center",
   width: "90%",
-  height: "100px",
+  height: "130px",
   borderRadius: "8px",
   backgroundColor: "#1e1e1e",
   marginTop: "20px",
+  padding: "10px",
 }));
 
 const CreatePlaylistButton = styled(Button)({
